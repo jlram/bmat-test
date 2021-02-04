e0180347d61ac9add777f344f85bd47f6f97cff4
jlram <joseluis.ramos@enreda.coop>
2021-02-04 19:11:19 +0100

    Corrected models and serializers

--- bmat_works/works/models.py
+++ bmat_works/works/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Work(models.Model):
     title = models.CharField(max_length=250, null=False, default='UNTITLED')
-    contributors = models.ManyToManyField('Contributor', related_name='contributors')
+    contributors = models.ManyToManyField('Contributor', related_name='contributors', blank=True)
     iswc = models.CharField(max_length=20, null=True)
-    sources = models.ManyToManyField('Source', related_name='labels')
+    sources = models.ManyToManyField('Source', related_name='labels', blank=True)
 
 
 class Contributor(models.Model):
--- bmat_works/works/serializers.py
+++ bmat_works/works/serializers.py
@@ -1,19 +1,19 @@
 from .models import Work, Contributor, Source
 from rest_framework import serializers
 
-class WorkSerializer(serializers.HyperlinkedModelSerializer):
+class WorkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Work
         fields = '__all__'
 
 
-class ContributorSerializer(serializers.HyperlinkedModelSerializer):
+class ContributorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contributor
         fields = '__all__'
 
 
-class SourceSerializer(serializers.HyperlinkedModelSerializer):
+class SourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Source
         fields = '__all__'
