1ba873a5e61a2b893cef71824f2d7799631fefa6
jlram <joseluis.ramos@enreda.coop>
2021-02-06 12:53:02 +0100

    GET Contributors test

--- bmat_works/works/tests.py
+++ bmat_works/works/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Work, Contributor
+from .serializers import WorkSerializer, ContributorSerializer
 
-# Create your tests here.
+
+# initialize the APIClient app
+client = Client()
+
+class GetContributorsTest(TestCase):
+    """Test for GET contributors"""
+
+    def setUp(self):
+        Contributor.objects.create(name='Jose Luis Ramos')
+        Contributor.objects.create(name='Luis Jose Ramos')
+        Contributor.objects.create(name='Jose Luis martinez')
+        Contributor.objects.create(name='Luis Jose Martinez')
+    
+    def test_get_contributors(self):
+        response = client.get(reverse('contributor-list'))
+        contributor_data = ContributorSerializer(Contributor.objects.all(), many=True).data
+        self.assertEqual(response.data, contributor_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
